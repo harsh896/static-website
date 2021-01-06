5b9b3225a6099e2b9d4595358f0852547561d525
Rushi-addweb <rushi.p.addweb@gmail.com>
2021-01-06 15:19:15 +0530

    buildspecs updated

--- terraform/role.tf
+++ terraform/role.tf
@@ -20,7 +20,10 @@ resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
   role       = aws_iam_role.eks.name
 }
-
+resource "aws_iam_role_policy_attachment" "AmazonEKSServicePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
+  role       = aws_iam_role.eks.name
+}
 resource "aws_iam_role_policy_attachment" "AmazonEKSVPCResourceController" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
   role       = aws_iam_role.eks.name
@@ -32,16 +35,21 @@ resource "aws_iam_role_policy" "eks_policy" {
 
   policy = <<-EOF
 {
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-        "Action": [
-            "eks:*"
-        ],
-        "Effect": "Allow",
-        "Resource": "*"
-        }
-    ]
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": "iam:CreateServiceLinkedRole",
+      "Resource": "arn:aws:iam::*:role/aws-service-role/*"
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "ec2:DescribeAccountAttributes"
+      ],
+      "Resource": "*"
+    }
+  ]
 }
 EOF
 }
