864644b771a76de4e63875c175aab891db025ad4
Rushi-addweb <rushi.p.addweb@gmail.com>
2021-01-07 11:57:12 +0530

    updated buildspec file

--- terraform/kube.tf
+++ terraform/kube.tf
@@ -45,10 +45,79 @@ resource "aws_s3_bucket_policy" "CodePipelineArtifactStore" {
 POLICY
 }
 
+
+
+data "aws_subnet_ids" "subnetIds" {
+  vpc_id = var.vpc_id
+}
+
+data "aws_subnet" "subnets" {
+  for_each = data.aws_subnet_ids.subnetIds.ids
+  id       = each.value
+}
+
+# output "subnet_cidr_blocks" {
+#   value = [for s in data.aws_subnet.subnets : s.id]
+# }
+
+
 resource "aws_ecr_repository" "ecr" {
   name = "kubernetes"
 }
+resource "aws_ecr_lifecycle_policy" "lifecycle_policy" {
+  repository = aws_ecr_repository.ecr.name
 
+  policy = <<EOF
+{
+  "rules": [
+    {
+      "rulePriority": 1,
+      "description": "Rentention upto 15 images",
+      "selection": {
+        "tagStatus": "any",
+        "countType": "imageCountMoreThan",
+        "countNumber": 15
+      },
+      "action": {
+        "type": "expire"
+      }
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_ecr_repository_policy" "repo_policy" {
+  repository = aws_ecr_repository.ecr.name
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "Stmt1598451708753",
+      "Principal": {
+          "AWS": "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+        },
+      "Action": [
+        "ecr:BatchCheckLayerAvailability",
+        "ecr:BatchGetImage",
+        "ecr:DescribeImages",
+        "ecr:DescribeRepositories",
+        "ecr:GetAuthorizationToken",
+        "ecr:GetDownloadUrlForLayer",
+        "ecr:GetLifecyclePolicy",
+        "ecr:GetLifecyclePolicyPreview",
+        "ecr:GetRepositoryPolicy",
+        "ecr:ListImages",
+        "ecr:ListTagsForResource"
+      ],
+      "Effect": "Allow"
+    }
+  ]
+}
+EOF
+}
 resource "aws_eks_cluster" "eks" {
   name     = "kubernetes-cluster"
   role_arn = aws_iam_role.eks.arn
@@ -63,15 +132,24 @@ resource "aws_eks_cluster" "eks" {
   ]
 }
 
-data "aws_subnet_ids" "subnetIds" {
-  vpc_id = var.vpc_id
+
+
+resource "aws_eks_node_group" "node" {
+  cluster_name    = aws_eks_cluster.eks.name
+  node_group_name = "node_tuto"
+  node_role_arn   = aws_iam_role.eks_nodes.arn
+  subnet_ids      = [for s in data.aws_subnet.subnets : s.id]
+
+  scaling_config {
+    desired_size = 1
+    max_size     = 1
+    min_size     = 1
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly,
+  ]
 }
 
-data "aws_subnet" "subnets" {
-  for_each = data.aws_subnet_ids.subnetIds.ids
-  id       = each.value
-}
-
-output "subnet_cidr_blocks" {
-  value = [for s in data.aws_subnet.subnets : s.id]
-}
--- terraform/role.tf
+++ terraform/role.tf
@@ -24,32 +24,39 @@ resource "aws_iam_role_policy_attachment" "AmazonEKSServicePolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
   role       = aws_iam_role.eks.name
 }
-# resource "aws_iam_role_policy_attachment" "AmazonEKSVPCResourceController" {
-#   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
-#   role       = aws_iam_role.eks.name
-# }
 
-resource "aws_iam_role_policy" "eks_policy" {
-  name = "eks_policy"
-  role = aws_iam_role.eks.id
 
-  policy = <<-EOF
+#--------------------------------------eks nodes-------------------------------------------------------
+resource "aws_iam_role" "eks_nodes" {
+  name = "eks-node-group-tuto"
+
+  assume_role_policy = <<POLICY
 {
   "Version": "2012-10-17",
   "Statement": [
     {
       "Effect": "Allow",
-      "Action": "iam:CreateServiceLinkedRole",
-      "Resource": "arn:aws:iam::*:role/aws-service-role/*"
-    },
-    {
-      "Effect": "Allow",
-      "Action": [
-        "ec2:DescribeAccountAttributes"
-      ],
-      "Resource": "*"
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
     }
   ]
 }
-EOF
+POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.eks_nodes.name
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.eks_nodes.name
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.eks_nodes.name
 }
